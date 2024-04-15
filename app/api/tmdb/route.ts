c19198d32e319c76e978f0d5830369cc2c7fe4ce
Kais Jessa <kaisjessa@gmail.com>
2024-04-15 11:45:09 -0400

    prevent duplicate movies

--- app/api/tmdb/route.ts
+++ app/api/tmdb/route.ts
@@ -14,7 +14,6 @@ export async function GET(request: Request): Promise<Response> {
     }
     const { searchParams } = new URL(request.url);
     const userQuery = searchParams.get("query");
-    console.log(userQuery);
     if (!userQuery) {
       throw Error("Missing query parameter");
     }
--- app/create/displayCreate.tsx
+++ app/create/displayCreate.tsx
@@ -4,6 +4,7 @@ import { Movie } from "../firebase/types";
 import Image from "next/image";
 import { Bounce, ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { unstable_noStore as noStore } from "next/cache";
 
 const Poster = (props: { movie: Movie }) => {
   const [loading, setLoading]: [boolean, any] = useState(true);
@@ -23,12 +24,18 @@ const Poster = (props: { movie: Movie }) => {
   );
 };
 
-const CategoryField = (props: { row: number }) => {
+const CategoryField = (props: { row: number; func: Function }) => {
+  const [userSearch, setUserSearch]: [string, any] = useState("");
   return (
     <input
       type="text"
       placeholder={`Enter category #${props.row}`}
       className="input input-bordered w-full"
+      value={userSearch}
+      onChange={(e) => {
+        setUserSearch(e.target.value);
+        props.func(e.target.value);
+      }}
     />
   );
 };
@@ -42,6 +49,7 @@ const MovieField = (props: { func: Function }) => {
         setData([]);
         return;
       }
+      noStore();
       const movieData = await fetch(`/api/tmdb?query=${userSearch}`);
       const movies = await movieData.json();
       setData(movies);
@@ -52,7 +60,7 @@ const MovieField = (props: { func: Function }) => {
   const onSelection = (m: Movie) => {
     setUserSearch("");
     setData([]);
-    props.func(m);
+    props.func(m, true);
   };
 
   return (
@@ -82,45 +90,19 @@ const MovieField = (props: { func: Function }) => {
   );
 };
 
-const MoviesBox = (props: { row: number }) => {
+const MoviesBox = (props: {
+  row: number;
+  setCat: Function;
+  setMovies: Function;
+  allSelections: Movie[][];
+}) => {
+  console.log(props.allSelections[props.row - 1]);
   const colours = [
     "border-blue-400",
     "border-green-400",
     "border-yellow-400",
     "border-red-400",
   ];
-  const [selections, setSelections]: [Movie[], any] = useState([]);
-  const updateSelections = (m: Movie) => {
-    if (selections.map((m) => m.id).includes(m.id)) {
-      toast.error("Movie already added!", {
-        position: "top-center",
-        autoClose: 1000,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        progress: 0,
-        theme: "colored",
-        transition: Bounce,
-      });
-      return;
-    }
-    if (selections.length == 4) {
-      toast.error("Category is full!", {
-        position: "top-center",
-        autoClose: 1000,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        progress: 0,
-        theme: "colored",
-        transition: Bounce,
-      });
-      return;
-    }
-    setSelections([...selections, m]);
-  };
 
   return (
     <div
@@ -129,21 +111,21 @@ const MoviesBox = (props: { row: number }) => {
       }`}
     >
       <div className="justify-center w-full font-bold">
-        <CategoryField row={props.row} />
+        <CategoryField row={props.row} func={props.setCat} />
       </div>
       <div className="justify-center w-full">
-        <MovieField func={updateSelections} />
+        <MovieField func={props.setMovies} />
       </div>
 
       <div className="grid grid-cols-4 grid-flow-row pb-1 relative rounded">
         <div className="relative col-span-4 grid grid-rows-1 grid-flow-col rounded"></div>
-        {selections.map((m, i) => (
+        {props.allSelections[props.row - 1].map((m, i) => (
           <button
             key={i}
             className="border-4 border-transparent rounded hover:opacity-20"
-            onClick={() =>
-              setSelections(selections.filter((mm) => m.id !== mm.id))
-            }
+            onClick={() => {
+              props.setMovies(m, false);
+            }}
           >
             <Poster movie={m} />
           </button>
@@ -154,6 +136,97 @@ const MoviesBox = (props: { row: number }) => {
 };
 
 const DisplayCreate = () => {
+  const [movieArray, setMovieArray]: [Movie[][], any] = useState(
+    new Array(4).fill([])
+  );
+  const [categoryArray, setCategoryArray]: [string[], any] = useState(
+    new Array(4).fill("")
+  );
+  const [title, setTitle]: [string, any] = useState("");
+  const [author, setAuthor]: [string, any] = useState("");
+  const [errMessage, setErrMessage]: [string, any] = useState(
+    "Error: Title cannot be empty"
+  );
+
+  useEffect(() => {
+    if (title.length === 0) {
+      setErrMessage("Error: Title cannot be empty");
+    } else if (author.length === 0) {
+      setErrMessage("Error: Your name cannot be empty");
+    } else if (
+      categoryArray.length < 4 ||
+      categoryArray.includes("") ||
+      new Set(categoryArray).size != categoryArray.length
+    )
+      setErrMessage("Error: Categories cannot be empty or repeat");
+    else if (
+      movieArray.flat().map((m) => m?.id).length < 16 ||
+      movieArray
+        .flat()
+        .map((m) => m?.id)
+        .some((el) => !el || el == undefined || el == null) ||
+      new Set(movieArray.flat().map((m) => m?.id)).size !=
+        movieArray.flat().length
+    )
+      setErrMessage("Error: Movies cannot be empty or repeat");
+    else setErrMessage("");
+    console.log(errMessage);
+  }, [categoryArray, movieArray, title, author, errMessage]);
+
+  const updateCategoryAtI = (i: number) => {
+    const updateCategories = (c: string) => {
+      setCategoryArray(categoryArray.map((cc, ii) => (ii == i ? c : cc)));
+    };
+    return updateCategories;
+  };
+  const updateMoviesAtI = (i: number) => {
+    const selections = movieArray[i];
+    const updateMovies = (m: Movie, add: boolean) => {
+      if (!add) {
+        setMovieArray(
+          movieArray.map((row, j) => row.filter((mm) => mm.id != m.id))
+        );
+      } else {
+        if (
+          movieArray
+            .flat()
+            .map((mm) => mm.id)
+            .includes(m.id)
+        ) {
+          toast.error("Movie already added!", {
+            position: "top-center",
+            autoClose: 1000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            progress: 0,
+            theme: "colored",
+            transition: Bounce,
+          });
+        } else if (selections.length == 4) {
+          toast.error("Category is full!", {
+            position: "top-center",
+            autoClose: 1000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            progress: 0,
+            theme: "colored",
+            transition: Bounce,
+          });
+          return;
+        } else {
+          setMovieArray(
+            movieArray.map((row, j) => (j == i ? [...row, m] : row))
+          );
+        }
+      }
+    };
+
+    return updateMovies;
+  };
   return (
     <div className="flex flex-col items-center justify-center relative rounded">
       <ToastContainer />
@@ -164,20 +237,39 @@ const DisplayCreate = () => {
             type="text"
             placeholder={`Puzzle Title`}
             className="input input-bordered w-full font-bold text-2xl"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
           />
           <input
             type="text"
             placeholder={`Your name`}
             className="input input-bordered w-full font-bold text-md"
+            value={author}
+            onChange={(e) => {
+              setAuthor(e.target.value);
+            }}
           />
         </div>
-        <MoviesBox row={1} />
-        <MoviesBox row={2} />
-        <MoviesBox row={3} />
-        <MoviesBox row={4} />
+        {[1, 2, 3, 4].map((i) => (
+          <MoviesBox
+            key={i}
+            row={i}
+            setCat={updateCategoryAtI(i - 1)}
+            setMovies={updateMoviesAtI(i - 1)}
+            allSelections={movieArray}
+          />
+        ))}
       </div>
       <div>
-        <button className="btn btn-secondary justify-center items-center">
+        <button
+          className={
+            errMessage.length === 0
+              ? "btn btn-secondary justify-center items-center"
+              : "btn btn-disabled justify-center items-center"
+          }
+        >
           Submit
         </button>
       </div>
